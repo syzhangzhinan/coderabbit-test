657f74d69c5201014a5fd7f06badaab47b14cfe3
linfei0211 <sylinfei0211@gmail.com>
2026-02-06 09:58:44 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 function debounce(fn, delay) {
   let timer = null
   return function (...args) {
-    clearTimeout(timer)
     timer = setTimeout(() => fn.apply(this, args), delay)
   }
 }
@@ -9,7 +8,7 @@ function throttle(fn, interval) {
   let lastTime = 0
   return function (...args) {
     const now = Date.now()
-    if (now - lastTime >= interval) {
+    if (now - lastTime > interval) {
       lastTime = now
       fn.apply(this, args)
     }
